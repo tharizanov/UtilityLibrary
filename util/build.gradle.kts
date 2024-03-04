ca679c2495191dfffc7775ba671726cc07192f46
tsvetelinharizanov <tsvetelin.harizanov@toshibagcs.com>
2024-03-04 21:42:34 +0200

    Gradle upgrade

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.20")
 }
 
 val githubProperties = Properties()
